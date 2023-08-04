0e2eeacd73b06de57404c3b29afb7210c2f9cf89
badscientist1312 <97522004+badscientist1312@users.noreply.github.com>
2023-08-04 15:37:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-bolt_api_key ="c684a560-8bbe-4a3d-9653-2a60db5385e3"
-device_id ="BOLT3568339"
+bolt_api_key ="Write your API key"
+device_id ="Your Bolt Device Id"
 telegram_chat_id ="@smart_alarm_pratyush"
 telegram_bot_id ="bot6581803984:AAGT9-q7X82Y0aGx0L9WkBmQwH3HMEJVVyQ"
