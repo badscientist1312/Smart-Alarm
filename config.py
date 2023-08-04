875008ef49536cca5772daa398e8be78f4b8bc9c
Pratyush <97522004+badscientist1312@users.noreply.github.com>
2023-08-04 18:45:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 bolt_api_key ="Write your API key"
 device_id ="Your Bolt Device Id"
-telegram_chat_id ="@smart_alarm_pratyush"
-telegram_bot_id ="bot6581803984:AAGT9-q7X82Y0aGx0L9WkBmQwH3HMEJVVyQ"
+telegram_chat_id ="@!your channel id!"
+telegram_bot_id ="bot!your bot id!"
